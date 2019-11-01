84ea0697a15051c892670c32cc80f850b4b1d7f0
adinoeleazar <adinoeleazar@gmail.com>
2019-11-01 09:15:48 +0700

    week 3

--- excercise-15.js
+++ excercise-15.js
@@ -1,27 +1,35 @@
 function groupAnimals(animals) {
 
-    // under construction
-    
-    var last = animals.length - 1
+
     var result = []
-    var ggwp
+    var animalsUrut = animals.sort()
+    var animalsAkhir = animals.length - 1
 
+    
+    for (var a = 0; a <= animalsAkhir; a++){
+        
+        var binatang = animalsUrut[a]
+        var resultAkhir = result.length - 1
+        var duplikat = false
+        
+        for (var b = 0; b <= resultAkhir; b++) {
+            if (binatang[0] === result[b][0][0]) {      
+                duplikat = true
+                result[b].push(binatang)
+            }
+        }
 
-    for (var a = 0; a < last; a++) {
-        result[a] = []
-        for (var b = 0 ; b <= last; b++){
-            if (animals[a][0] === animals[b][0]) {result[a].push(animals[b])}
+        if (duplikat === false){                        
+            result.push([binatang]);
         }
     }
 
-    for (var c = 0; c < result.length - 1; c++) {
-        for (var d = c+1; d < result.length; d++) {
-            if (result[c] === [result[d]]) {ggwp = [result.splice(c, 1)]}
-        }
-    }
-
-    return ggwp
+    return result;
 }
+
+    
+
+
   
   // TEST CASES
   console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
